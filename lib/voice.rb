271527c46f3f61d8146d75992676e47c307bf37b
Jacob Helwig <jacobh@elementaltechnologies.com>
2013-08-24 18:00:07 -0700

    Don't rely on cut to handle voices with multiple words
    Using `say -v ? | cut -f1 -d' '` to get the list of voices cuts off
    everything after the first word in voices with multiple words in them.
    This is fine in the case where the first word is unambiguous as the say
    command handles the shortened form.  However, we should not rely on this
    being unambiguous.

--- lib/voice.rb
+++ lib/voice.rb
@@ -12,7 +12,14 @@ module Voice
   ]
 
   if PLATFORM_IS_OSX
-    @@all = `say -v ? | cut -f1 -d' '`.split("\n")
+    @@all = []
+    `say -v ?`.each_line do |l|
+      # Each line resembles: Agnes               en_US    # Isn't it nice to have a computer that will talk to you?
+      voice = l.split('#')[0]. # Grab everything before the comment marker
+        strip.         # Drop the extra spaces at the end
+        split(' ')     # Split into words
+      @@all << voice[0..-2].join(' ') # Drop the last word (locale), and join the rest with a single space
+    end
     @@novelty = @@all.select{|v| NOVELTY_VOICES.include?(v)}
     @@all = @@all - @@novelty
   else
